9b62a1c9c33978dcbffb5c15c8d7ea4a825abb2a
scda <dsop@mailbox.org>
2019-09-25 16:02:26 +0200

    add comments on exported components

--- config/config.go
+++ config/config.go
@@ -1,5 +1,6 @@
 package config
 
+// current setup for the constant variables
 const (
 	DbType = "sqlite3"
 	DbFile = "test.db"
--- models/base.go
+++ models/base.go
@@ -4,6 +4,7 @@ import (
 	"../database"
 )
 
+// Initialize will add model schema to the database
 func Initialize() {
 	database.DB.AutoMigrate(
 		&User{},
